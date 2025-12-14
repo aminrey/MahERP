9fc0f5a9b1d36ab03b9cefcb8f5c305bce7a46b9
amin rey <amin_deadly@live.com>
2025-12-14 17:56:56 +0330

    migration

---
+++ MahERP.DataModelLayer/Migrations/20251214142208_settings.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MahERP.DataModelLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class settings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CanEditSettingsRoles",
+                table: "TaskSettings_Tbl",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CanEditSettingsRoles",
+                table: "TaskCategoryDefaultSettings_Tbl",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CanEditSettingsRoles",
+                table: "BranchDefaultTaskSettings_Tbl",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CanEditSettingsRoles",
+                table: "TaskSettings_Tbl");
+
+            migrationBuilder.DropColumn(
+                name: "CanEditSettingsRoles",
+                table: "TaskCategoryDefaultSettings_Tbl");
+
+            migrationBuilder.DropColumn(
+                name: "CanEditSettingsRoles",
+                table: "BranchDefaultTaskSettings_Tbl");
+        }
+    }
+}
