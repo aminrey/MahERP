6fac248df7b7148dab6df8683785279115f4ab2c
hosein reyshahri <hoseinsmart@hotmail.com>
2025-12-27 18:23:56 +0330

    1

---
+++ MahERP.DataModelLayer/Migrations/20251227143422_AddSeeDdta.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MahERP.DataModelLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeeDdta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Branch_Tbl",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "شعبه مرکزی ", "شعبه مرکزی" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Branch_Tbl",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Description", "Name" },
+                values: new object[] { "شعبه برند رسنا", "شعبه رسنا" });
+        }
+    }
+}
